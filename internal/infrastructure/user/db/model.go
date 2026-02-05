f8e0389dbec3d31f29c5b51f409cb9fe5cd74179
liziyue <liziyue256@gmail.com>
2026-02-05 16:54:49 +0800

    全流程调试到入库demo完成

--- internal/infrastructure/user/db/model.go
+++ internal/infrastructure/user/db/model.go
@@ -1,11 +1,16 @@
 package db
 
-import "gorm.io/gorm"
+import (
+    // "gorm.io/gorm"
+    "time"
+)
 
 type UserModel struct {
-    gorm.Model
-    Username string `gorm:"unique;not null"`
-    Password string `gorm:"not null"`
+    ID        uint      `gorm:"primaryKey"`
+    Username  string    `gorm:"unique;not null"`
+    Password  string    `gorm:"not null"`
+    CreatedAt time.Time `gorm:"autoCreateTime"`
+    UpdatedAt time.Time `gorm:"autoUpdateTime"`
 }
 
 func (UserModel) TableName() string {
